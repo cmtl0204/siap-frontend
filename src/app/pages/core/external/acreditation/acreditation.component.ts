4931c3ce548af24485676c7b403481e891cc779e
cmtl0204 <cesar.tamayo0204@gmail.com>
2025-04-30 19:51:12 -0500

    fix bugs

---
+++ src/app/pages/core/external/acreditation/acreditation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acreditation',
+  imports: [],
+  templateUrl: './acreditation.component.html',
+  styleUrl: './acreditation.component.scss'
+})
+export class AcreditationComponent {
+
+}
---
+++ src/app/pages/core/external/steps/step1/step1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step1',
+  imports: [],
+  templateUrl: './step1.component.html',
+  styleUrl: './step1.component.scss'
+})
+export class Step1Component {
+
+}
