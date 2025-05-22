11cf7eb283832b5c31a4de242f2aa33a5b053cd0
cmtl0204 <cesar.tamayo0204@gmail.com>
2025-05-22 09:31:55 -0500

    fix bugs

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuItem, PrimeIcons } from 'primeng/api';
 import { AppMenuitem } from './app.menuitem';
 import { MY_ROUTES } from '@routes';
+import { AuthService } from '@modules/auth/auth.service';
+import { Ripple } from 'primeng/ripple';
 
 @Component({
     selector: 'app-menu',
     standalone: true,
-    imports: [CommonModule, AppMenuitem, RouterModule],
+    imports: [CommonModule, AppMenuitem, RouterModule, Ripple],
     template: `
         <ul class="layout-menu">
             <ng-container *ngFor="let item of model; let i = index">
@@ -16,10 +18,20 @@ import { MY_ROUTES } from '@routes';
                 <li *ngIf="item.separator" class="menu-separator"></li>
             </ng-container>
         </ul>
+        <div class="mt-auto">
+            <hr class="mb-4 mx-4 border-t border-0 border-surface" />
+
+            <a (click)="signOut()" pRipple class="m-4 flex items-center cursor-pointer p-4 gap-2 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors p-ripple">
+                <i [class]="PrimeIcons.POWER_OFF" style="color:red "></i>
+                <span class="font-bold" style="color: red"> Cerrar Sesión </span>
+            </a>
+        </div>
     `
 })
-export class AppMenu {
+export class AppMenu implements OnInit {
     model: MenuItem[] = [];
+    protected readonly PrimeIcons = PrimeIcons;
+    protected readonly authService = inject(AuthService);
 
     ngOnInit() {
         this.model = [
@@ -41,7 +53,11 @@ export class AppMenu {
                         routerLink: [MY_ROUTES.corePages.manager.project.list.absolute]
                     }
                 ]
-            },
+            }
         ];
+    }
+
+    signOut() {
+        this.authService.removeLogin();
     }
 }
--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component, inject } from '@angular/core';
+import { MenuItem,PrimeIcons } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
 import { AppConfigurator } from './app.configurator';
 import { LayoutService } from '../service/layout.service';
 import { environment } from '@env/environment';
+import { Button } from 'primeng/button';
+import { AuthService } from '@modules/auth/auth.service';
 
 @Component({
     selector: 'app-topbar',
     standalone: true,
-    imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator],
+    imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator, Button],
     template: ` <div class="layout-topbar">
         <div class="layout-topbar-logo-container">
             <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
@@ -34,49 +36,20 @@ import { environment } from '@env/environment';
                         />
                     </g>
                 </svg>
-                <span>{{environment.APP_SHORT_NAME}}</span>
+                <span>{{ environment.APP_SHORT_NAME }}</span>
             </a>
         </div>
 
         <div class="layout-topbar-actions">
-            <div class="layout-config-menu">
-                <button type="button" class="layout-topbar-action" (click)="toggleDarkMode()">
-                    <i [ngClass]="{ 'pi ': true, 'pi-moon': layoutService.isDarkTheme(), 'pi-sun': !layoutService.isDarkTheme() }"></i>
-                </button>
-                <div class="relative">
-                    <button
-                        class="layout-topbar-action layout-topbar-action-highlight"
-                        pStyleClass="@next"
-                        enterFromClass="hidden"
-                        enterActiveClass="animate-scalein"
-                        leaveToClass="hidden"
-                        leaveActiveClass="animate-fadeout"
-                        [hideOnOutsideClick]="true"
-                    >
-                        <i class="pi pi-palette"></i>
-                    </button>
-                    <app-configurator />
-                </div>
-            </div>
-
             <button class="layout-topbar-menu-button layout-topbar-action" pStyleClass="@next" enterFromClass="hidden" enterActiveClass="animate-scalein" leaveToClass="hidden" leaveActiveClass="animate-fadeout" [hideOnOutsideClick]="true">
                 <i class="pi pi-ellipsis-v"></i>
             </button>
 
             <div class="layout-topbar-menu hidden lg:block">
                 <div class="layout-topbar-menu-content">
-                    <button type="button" class="layout-topbar-action">
-                        <i class="pi pi-calendar"></i>
-                        <span>Calendar</span>
-                    </button>
-                    <button type="button" class="layout-topbar-action">
-                        <i class="pi pi-inbox"></i>
-                        <span>Messages</span>
-                    </button>
-                    <button type="button" class="layout-topbar-action">
-                        <i class="pi pi-user"></i>
-                        <span>Profile</span>
-                    </button>
+                    <p-button type="button" [text]="true" [raised]="true" [label]="_authService.auth.username" [icon]="PrimeIcons.USER" />
+
+                    <p-button (onClick)="signOut()" type="button" [text]="true" [raised]="true" severity="danger" label="Cerrar Sesión" [icon]="PrimeIcons.POWER_OFF" />
                 </div>
             </div>
         </div>
@@ -84,6 +57,8 @@ import { environment } from '@env/environment';
 })
 export class AppTopbar {
     items!: MenuItem[];
+    protected readonly _authService = inject(AuthService);
+    protected readonly PrimeIcons = PrimeIcons;
 
     constructor(public layoutService: LayoutService) {}
 
@@ -91,5 +66,8 @@ export class AppTopbar {
         this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
     }
 
+    signOut() {
+        this._authService.removeLogin();
+    }
     protected readonly environment = environment;
 }
